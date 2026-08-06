bd761229976f20ff52e507a09a405790b69f5c9f
이경인 <031317ki@gmail.com>
2026-08-06 20:44:26 +0900

    [Feat] BattleManager, PlayerParty 스크립트 구성

--- Assets/02_Scripts/Manager/BattleManager.cs
+++ Assets/02_Scripts/Manager/BattleManager.cs
@@ -1,16 +1,35 @@
-using UnityEngine;
+﻿using UnityEngine;
 
 public class BattleManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public static BattleManager Instance { get; private set; }
+
+    private void Awake()
     {
-        
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+    public void StartBattle(PlayerParty playerParty, GameObject monsterParty)
+    {
+        Debug.Log("전투 시작!");
+
+        //TODO: 전투 로직 구현
+
+        EndBattle(playerParty, monsterParty);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void EndBattle(PlayerParty playerParty, GameObject monsterParty)
     {
-        
+        monsterParty.SetActive(false);
+        Debug.Log("전투 종료!");
+
+        playerParty.isBattling = false;
     }
 }
--- Assets/02_Scripts/Manager/ObjectManager.cs
+++ Assets/02_Scripts/Manager/ObjectManager.cs
@@ -1,16 +1,27 @@
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 public class ObjectManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public static ObjectManager Instance { get; private set; }
+
+    private void Awake()
     {
-        
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [SerializeField] private GameObject Prefab_Hunter;
+    [SerializeField] private GameObject Prefab_Enemy;
+    [SerializeField] private Transform Root_Enemy;
+
+    private int _objectInstanceKeyGenerator = 0;
+
+
 }
