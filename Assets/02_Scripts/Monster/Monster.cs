9e6d6d068e2c5485e92d248764f4c6766cd95e63
권성혁 <gkrrpdl@gmail.com>
2026-08-06 14:52:13 +0900

    [fix] 몬스터 코드 수정

--- Assets/02_Scripts/Monster/Monster.cs
+++ Assets/02_Scripts/Monster/Monster.cs
@@ -4,17 +4,23 @@ using UnityEngine;
 public class Monster : MonoBehaviour
 {
     [Header("스탯 관련")]
+    private int _monsterMaxHp;
     private int _monsterHp;
     private int _monsterMp;
     private int _monsterAtkSpeed;
+    private int _monsterAtk;
 
     private Animator _monsterAnimator;
 
-    private Action<int> _onChangedHp;
+    private Action<int, int> _onChangedHp;
 
     private void Awake()
     {
         //[TODO] 데이터 불러오기
+        _monsterHp = 100;
+        _monsterAtkSpeed = 10;
+        _monsterAtk = 30;
+        this.gameObject.SetActive(true);
     }
 
     private void OnEnable()
@@ -22,9 +28,15 @@ public class Monster : MonoBehaviour
         //[TODO] Hud 생성, 오브젝트매니저에 캐릭터 등록(소통후)
     }
 
-    private void AtkTarget()
+    private void AtkTarget(string targetId)
     {
         //[TODO] 평타공격
+        // var data = GameDataManager.Inst.GetData(targetId);
+        if (targetId == "data.Id")  // 포멧으로 데이터 확인
+        {
+            // var target = GameObjectManager.Inst.GetTarget(targetId);
+            // target.TakeDamage(_monsterAtk);
+        }
     }
 
     private void TakeDamage(int damage)
@@ -40,6 +52,7 @@ public class Monster : MonoBehaviour
     private void Die()
     {
         //[TODO] 죽음 애니메이션 재생
+        this.gameObject.SetActive(false);
     }
 
     private void ChangeState()
@@ -47,13 +60,18 @@ public class Monster : MonoBehaviour
         //[TODO] 애니메이션 변경 (평타공격, 죽는애니메이션)
     }
 
-    private void ResetState()
+    public void BindOnStatChangedEvent(Action<int, int> hpChangeCallback)
     {
-        //[TODO] 스탯리셋
+        _onChangedHp += hpChangeCallback;
     }
 
-    private void OnChangeHP(int monsterHp)
+    private void ResetStateChangedEvent()
     {
-        _monsterHp = monsterHp;
+        _onChangedHp = null;
+    }
+
+    private void InvokeStatChangedEvent()
+    {
+        _onChangedHp?.Invoke(_monsterHp, _monsterMaxHp);
     }
 }
