6aec8d08550089abe3464aa9729f780f05bba66f
지재욱 <145469443+wlwodnr@users.noreply.github.com>
2026-08-10 01:18:42 +0900

    [feat] 골드 프로퍼티 추가

---
+++ Assets/02_Scripts/Ui/PlayerResourceViewModel.cs
@@ -0,0 +1,16 @@
+﻿public class PlayerResourceViewModel : ViewModelBase
+{
+    private long _gold;
+    public long Gold
+    {
+        get => _gold;
+        set
+        {
+            if (_gold != value)
+            {
+                _gold = value;
+                OnPropertyChanged(nameof(Gold));
+            }
+        }
+    }
+}
