100c9bf5ae64b95bf6b99b3e9553c0bfc7da65a8
지재욱 <145469443+wlwodnr@users.noreply.github.com>
2026-08-10 01:27:24 +0900

    [feat] 폴더 이동 및 플레이어 재화 서비스 구현 (saveManager와 연동 해야함)

---
+++ Assets/02_Scripts/Ui/MVVM/PlayerResourceService.cs
@@ -0,0 +1,41 @@
+﻿public class PlayerResourceService 
+{
+    private PlayerResourceViewModel _playerResourceViewModel;
+
+    public PlayerResourceViewModel GetPlayerResourceViewModel()
+    {
+        if(_playerResourceViewModel == null)
+        {
+            CreatePlayerResourceViewModel();
+        }
+        return _playerResourceViewModel;
+    }
+
+    private PlayerResourceViewModel CreatePlayerResourceViewModel()
+    {
+        var resourceViewModel = new PlayerResourceViewModel();
+
+        _playerResourceViewModel = resourceViewModel;
+
+        return resourceViewModel;
+    }
+
+    public void SetGoldOnLoad(long gold)
+    {
+        var resourceViewModel = GetPlayerResourceViewModel();
+
+        resourceViewModel.Gold = gold;
+    }
+
+    public void RequestAddGold(long addGold)
+    {
+        if(addGold <= 0)
+        {
+            return;
+        }
+
+        var resourceViewModel = GetPlayerResourceViewModel();
+
+        resourceViewModel.Gold += addGold;
+    }
+}
