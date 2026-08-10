75848fdfee5e443125f62522a8262fa4a720a9d4
권성혁 <gkrrpdl@gmail.com>
2026-08-10 18:08:16 +0900

    [Fix] 스킬코스트 수정 함수 추가

--- Assets/02_Scripts/GameObject/Character/Character.cs
+++ Assets/02_Scripts/GameObject/Character/Character.cs
@@ -5,9 +5,9 @@ using UnityEngine;
 public class Character : BattleCharacter
 {
     [Header("스킬 관련")]
-    [SerializeField] private Collider Collider_Skill;
-    [SerializeField] private GameObject Prefab_Skill;
-    [SerializeField] private Transform Root_SkillSpawn;
+    private int _RequiredSkillCost;
+    private int _currentSkillCost;
+    private int _MaxSkillCost;
 
     [Header("데이터 관련")]
     private CharacterData _characterData;
@@ -19,8 +19,6 @@ public class Character : BattleCharacter
 
     private Animator _characterAnimator;
 
-    
-
     private bool _isSkillUsable = false;
     private void Awake()
     {
@@ -29,6 +27,7 @@ public class Character : BattleCharacter
 
     private void OnEnable()    // [TODO] 우선 Start로 하고 동적생성이 되면 OnEnable로 변경
     {
+        _currentSkillCost = 0;
 
         //[TODO] Hud 생성, 오브젝트매니저에 캐릭터 등록(소통후)
         if (GameDataManager.Instance == null)
@@ -38,6 +37,7 @@ public class Character : BattleCharacter
         //_characterData = GameDataManager.Instance.GetCharacterData("character_Test_01");     // [TODO] 하드코딩을 하지않고 (ID)데이터를 받아와야함
         _characterData = GameDataManager.Instance.GetData<CharacterData>("character_Test_01");
         _characterId = _characterData.Id;
+        _MaxSkillCost = _characterData.MaxSkillCost;
         SetStatData();
     }
 
@@ -64,6 +64,7 @@ public class Character : BattleCharacter
 
         if (_isSkillUsable == true)
         {
+            //[TODO] 스킬사용 모션
             _skill.UseSkill();
         }
     }
@@ -74,9 +75,38 @@ public class Character : BattleCharacter
 
         if (_isSkillUsable == false)
         {
+            UseNormalAttack(targetMonster);
             Debug.Log($"타겟{targetMonster.name}에게 {_characterAtk} 데미지를 줍니다.");
-            //[TODO] 평타공격 모션
-            targetMonster.TakeDamage(_characterAtk);
+            return;
         }
+        else if (_isSkillUsable == true && _RequiredSkillCost <= _currentSkillCost)
+        {
+            UseSkillCost(_skill.GetRequiredSkillCost());
+            UseSkill();
+        }
+    }
+
+    private void UseNormalAttack(Monster targetMonster)
+    {
+        //[TODO] 평타공격 모션
+        targetMonster.TakeDamage(_characterAtk);
+    }
+
+    public void IncreaseCurrentSkillCost(int amount)
+    {
+        _currentSkillCost += amount;
+        if (_currentSkillCost > _MaxSkillCost)
+        {
+            _currentSkillCost = _MaxSkillCost;
+        }
+    }
+
+    public void UseSkillCost(int amount)
+    {
+        if (_currentSkillCost < amount)
+        {
+            Debug.Log("스킬 코스트가 부족합니다!");
+        }
+        _currentSkillCost -= amount;
     }
 }
