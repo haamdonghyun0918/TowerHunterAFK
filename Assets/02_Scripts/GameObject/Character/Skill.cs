4976b451a5285b4323a853fab38ba3825d59c01d
권성혁 <gkrrpdl@gmail.com>
2026-08-10 18:08:47 +0900

    [Fix] 스킬코스트 Get, Set 함수 추가

--- Assets/02_Scripts/GameObject/Character/Skill.cs
+++ Assets/02_Scripts/GameObject/Character/Skill.cs
@@ -5,21 +5,30 @@ public class Skill : MonoBehaviour
 {
     private string _skillId;
     private string _skillPrefabPath;
-    private string _skillType;
+    private int _RequiredSkillCost;
     private SkillData _skillData;
-    private CharacterData _characterData;
-    private Character _character;
     private Transform _targetTransform;
 
     private void OnEnable()
     {
         _skillData = GameDataManager.Instance.GetData<SkillData>(_skillId);
         SetPrefabPath();
+        SetRequiredSkillCost();
     }
 
     public void SetSkillId(string skillId)
     {
         _skillId = skillId;
+    }
+
+    private void SetRequiredSkillCost()
+    {
+        _RequiredSkillCost = _skillData.RequiredCost;
+    }
+
+    public int GetRequiredSkillCost()
+    {
+        return _RequiredSkillCost;
     }
 
     public void UseSkill()
