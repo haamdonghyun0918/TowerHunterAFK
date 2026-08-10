4f41f0d490bd26654dcae1d3c6b2d4b891610f71
권성혁 <gkrrpdl@gmail.com>
2026-08-10 16:58:22 +0900

    [Add] 배틀캐릭터 공용 클래스 추가

---
+++ Assets/02_Scripts/GameObject/Character/BattleCharacter.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class BattleCharacter : MonoBehaviour
+{
+    [Header("공통스탯")]
+    protected int _characterHp;
+    protected int _characterMaxHp;
+    protected int _characterAtk;
+    protected int _characterAtkSpeed;
+}
