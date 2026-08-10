d4ea7b98cf9335f56284396b8542376e6f8ea624
권성혁 <gkrrpdl@gmail.com>
2026-08-10 16:06:41 +0900

    [Fix] 캐릭터 공격 함수 수정 (Monster를 매개변수로 사용) 및 멤버변수명 변경

--- Assets/02_Scripts/GameObject/Character/Character.cs
+++ Assets/02_Scripts/GameObject/Character/Character.cs
@@ -19,6 +19,7 @@ public class Character : MonoBehaviour
 
     [Header("데이터 관련")]
     private CharacterData _characterData;
+    private string _characterId;
 
     [Header("전투 관련")]
     [SerializeField] private GameObject TargetMonster;
@@ -29,16 +30,18 @@ public class Character : MonoBehaviour
     private Action<int, int> _onChangedHp;
     private Action<int, int> _onChangedMp;
 
-    private bool _isCoolTime = false;
-    private bool _isDead = false;
+    private bool _isSkillUsable = false;
+    public bool _isDead { get; private set; }
 
     private void Awake()
     {
-        _isDead = false;
+
     }
 
-    private void Start()    // [TODO] 우선 Start로 하고 동적생성이 되면 OnEnable로 변경
+    private void OnEnable()    // [TODO] 우선 Start로 하고 동적생성이 되면 OnEnable로 변경
     {
+        _isDead = false;
+
         //[TODO] Hud 생성, 오브젝트매니저에 캐릭터 등록(소통후)
         if (GameDataManager.Instance == null)
         {
@@ -46,22 +49,19 @@ public class Character : MonoBehaviour
         }
         //_characterData = GameDataManager.Instance.GetCharacterData("character_Test_01");     // [TODO] 하드코딩을 하지않고 (ID)데이터를 받아와야함
         _characterData = GameDataManager.Instance.GetData<CharacterData>("character_Test_01");
+        _characterId = _characterData.Id;
         _targetMonsterComponent = TargetMonster.GetComponentInChildren<Monster>();    // [TODO] 타겟 몬스터 정하는 방식 정해야함
         SetStatData();
     }
 
-    private void Update()
+    public string GetCharacterId()
     {
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            AtkTarget();
-        }
+        return _characterId;
     }
 
     private void SetStatData()
     {
         var baseStatData = GameDataManager.Instance.GetData<BaseStatData>(_characterData.BaseStatDataId);
-        //var baseStatData = GameDataManager.Instance.GetBaseStatData(_characterData.BaseStatDataId);
 
         _characterAtk = baseStatData.BaseAtk;
         _characterAtkSpeed = baseStatData.BaseAtkSpeed;
@@ -74,26 +74,22 @@ public class Character : MonoBehaviour
     private void UseSkill()
     {
         string skillId = _characterData.Skill;
-        if (_isCoolTime == false) 
+        if (_isSkillUsable == true)
         {
-            //[TODO] 스킬사용
+            //[TODO] 스킬사용 모션
             //UseSkill(skillId);
         }
     }
 
-    private void AtkTarget()
+    private void AtkTarget(Monster targetMonster)
     {
-        if (_isDead == true) return;
+        if (targetMonster._isDead == true) return;
 
-        if (_targetMonsterComponent == null)
+        if (_isSkillUsable == false)
         {
-            Debug.Log("타겟 몬스터의 컴포넌트를 받아오지 못했습니다.");
-        }
-        _targetMonsterComponent.TakeDamage(_characterAtk);
-        Debug.Log($"타겟에게 {_characterAtk} 데미지를 줍니다.");
-        if (_isCoolTime == true)
-        {
-            //[TODO] 평타공격
+            Debug.Log($"타겟에게 {_characterAtk} 데미지를 줍니다.");
+            //[TODO] 평타공격 모션
+            targetMonster.TakeDamage(_characterAtk);
         }
     }
 
