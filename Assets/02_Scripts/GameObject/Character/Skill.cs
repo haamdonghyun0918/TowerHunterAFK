caa4bbcba997877776f46234f3d3b4e284cd4fa8
권성혁 <gkrrpdl@gmail.com>
2026-08-11 18:45:01 +0900

    [Fix] 스킬 클래스에 string으로 받은 스킬타입 enum으로 변경하는 함수 추가

--- Assets/02_Scripts/GameObject/Character/Skill.cs
+++ Assets/02_Scripts/GameObject/Character/Skill.cs
@@ -3,10 +3,20 @@ using UnityEngine;
 using UnityEngine.AddressableAssets;
 using UnityEngine.ResourceManagement.AsyncOperations;
 
+public enum SkillType
+{
+    None,
+    SingleTarget,
+    MultiTarget,
+    SelfTarget,
+    TeamTarget
+}
+
 public class Skill : MonoBehaviour
 {
     private string _skillId;
     private string _skillPrefabPath;
+    private string _skillType;
     private int _skillDamage;
     private int _requiredSkillCost;
     private SkillData _skillData;
@@ -29,6 +39,8 @@ public class Skill : MonoBehaviour
         SetBaseSkillDamage();
         SetPrefabPath();
         SetRequiredSkillCost();
+        TestSetTargetTransform();
+        SetSkillType();
     }
 
     private void SetSkillId(string skillId)
@@ -105,15 +117,69 @@ public class Skill : MonoBehaviour
         return _skillDamage;
     }
 
-    public string GetSkillType()
+    private SkillType SetSkillType()
     {
-        if (_skillData == null) return null;
-        return _skillData.SkillType;
+        if (_skillData == null) return SkillType.None;
+        _skillType = _skillData.SkillType;
+
+        try
+        {
+            return (SkillType)System.Enum.Parse(typeof(SkillType), _skillType, true);
+        }
+
+        catch
+        {
+            Debug.LogError($"[Skill] {_skillType}스킬타입 변환 실패");
+            return SkillType.None;
+        }
+    }
+
+    public SkillType GetSkillType()
+    {
+        if (_skillData == null) return SkillType.None;
+
+        try
+        {
+            return (SkillType)System.Enum.Parse(typeof(SkillType), _skillType, true);
+        }
+
+        catch
+        {
+            Debug.LogError($"[Skill] {_skillType}스킬타입 변환 실패");
+            return SkillType.None;
+        }
     }
 
     public Transform SetTargetTransform(Transform targetTransform)
     {
         _targetTransform = targetTransform;
         return _targetTransform;
+    }
+
+
+    // 테스트용 함수 ==================================================
+
+    private Transform TestSetTargetTransform()
+    {
+        if (_skillData == null) return null;
+
+        var skillType = GetSkillType();
+        switch (skillType)
+        {
+            case SkillType.SelfTarget:
+                {
+                    _targetTransform = this.gameObject.transform;
+                    return _targetTransform;
+                }
+            case SkillType.SingleTarget:
+                {
+                    _targetTransform = this.gameObject.transform;
+                    return _targetTransform;
+                }
+            default:
+                {
+                    return null;
+                }
+        }
     }
 }
