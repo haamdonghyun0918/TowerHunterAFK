ce42b04b32f65db40807a6579f000de91c2cd379
권성혁 <gkrrpdl@gmail.com>
2026-08-11 11:23:55 +0900

    [Fix] 스킬 클래스 스킬 동적생성 함수 비동기로 변경

--- Assets/02_Scripts/GameObject/Character/Skill.cs
+++ Assets/02_Scripts/GameObject/Character/Skill.cs
@@ -1,42 +1,91 @@
-﻿using UnityEngine;
+﻿using Cysharp.Threading.Tasks;
+using UnityEngine;
 using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
 
 public class Skill : MonoBehaviour
 {
     private string _skillId;
     private string _skillPrefabPath;
     private int _skillDamage;
-    private int _RequiredSkillCost;
+    private int _requiredSkillCost;
     private SkillData _skillData;
     private Transform _targetTransform;
 
     private void OnEnable()
     {
-        _skillData = GameDataManager.Instance.GetData<SkillData>(_skillId);
-        SetPrefabPath();
-        SetRequiredSkillCost();
-        SetSkillDamage();
+
     }
 
-    public void SetSkillId(string skillId)
+    public void InitializeSkill(string skillId)
+    {
+        SetSkillId(skillId);
+        _skillData = GameDataManager.Instance.GetData<SkillData>(_skillId);
+        if (_skillData == null)
+        {
+            Debug.LogError($"[Skill] 스킬 데이터를 불러오지 못했습니다.");
+            return;
+        }
+        SetBaseSkillDamage();
+        SetPrefabPath();
+        SetRequiredSkillCost();
+    }
+
+    private void SetSkillId(string skillId)
     {
         _skillId = skillId;
     }
 
     private void SetRequiredSkillCost()
     {
-        _RequiredSkillCost = _skillData.RequiredCost;
+        if (_skillData == null) return;
+        _requiredSkillCost = _skillData.RequiredCost;
     }
 
     public int GetRequiredSkillCost()
     {
-        return _RequiredSkillCost;
+        if (_skillData == null)
+        {
+            Debug.LogError($"[Skill] 스킬 데이터를 불러오지 못했습니다.");
+            return 0;
+        }
+        return _requiredSkillCost;
     }
 
-    public void UseSkill()
+    public async UniTaskVoid UseSkillAsync()
     {
-        if (_skillData == null) return;
-        Addressables.InstantiateAsync(_skillPrefabPath, _targetTransform);
+        if (_skillData == null)
+        {
+            Debug.LogError($"[Skill] 스킬 데이터가 초기화 되지 않았습니다.");
+            return;
+        }
+
+        GameObject instance = await InstantiateAsync(_skillPrefabPath, _targetTransform);
+
+    }
+
+    private async UniTask<GameObject> InstantiateAsync(string prefabPath, Transform parentTransform = null)
+    {
+        AsyncOperationHandle<GameObject> handle = Addressables.InstantiateAsync(prefabPath, parentTransform);
+
+        try
+        {
+            GameObject instance = await handle.ToUniTask();
+
+            return instance;
+        }
+
+        catch(System.Exception e)
+        {
+            Debug.LogError($"[Skill] 스킬 프리팹 생성 실패: {prefabPath} / 에러: {e.Message}");
+
+            if (handle.IsValid())
+            {
+                Addressables.Release(handle);
+            }
+
+            return null;
+        }
     }
 
     private void SetPrefabPath()
@@ -45,7 +94,7 @@ public class Skill : MonoBehaviour
         _skillPrefabPath = _skillData.PrefabPath;
     }
 
-    private void SetSkillDamage()
+    private void SetBaseSkillDamage()
     {
         if (_skillData == null) return;
         _skillDamage = _skillData.SkillDamage;
