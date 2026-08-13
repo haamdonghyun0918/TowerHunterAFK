00c3d78beea42d1b99333d2dda36bd6f1abcdef7
지재욱 <145469443+wlwodnr@users.noreply.github.com>
2026-08-13 23:10:55 +0900

    [Feat] 캐릭터 스텟 모델

---
+++ Assets/02_Scripts/Ui/MVVM/CharacterStatusModel.cs
@@ -0,0 +1,11 @@
+﻿public class CharacterStatusModel
+{
+    public int SlotIndex {  get; set; }
+    public int CharacterId { get; set; } = "";
+    public int CurrentHp { get; set; }
+    public int MaxHp { get; set; }
+    public int CurrentSkillCost { get; set; }
+    public int MaxSkillCost { get; set; }
+    public bool IsActive { get; set; }
+    public bool isDead { get; set; }
+}
