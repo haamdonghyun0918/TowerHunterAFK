19d4a43ae35c98f561508ac829f4fec143508c40
지재욱 <145469443+wlwodnr@users.noreply.github.com>
2026-08-14 01:28:26 +0900

    [Feat] 캐릭터 스테이터스 서비스 기초 구현

---
+++ Assets/02_Scripts/Ui/MVVM/CharacterStatusService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterStatusService
+{
+    private const int MaxPartyCount = 3;
+
+    private CharacterStatusModel[] _characterStatusModels = new CharacterStatusModel[MaxPartyCount];
+    private CharacterStatusViewModel[] _characterStatusViewModels = new CharacterStatusViewModel[MaxPartyCount];
+    private Character[] _characters = new Character[MaxPartyCount];
+
+    private Action<int, int>[] _hpChangedCallbacks = new Action<int, int>[MaxPartyCount];
+    private Action<int, int>[] _skillCostChangedCallbacks = new Action<int, int>[MaxPartyCount];
+
+    public CharacterStatusService()
+    {
+        
+    }
+    private void CreateCharacterStatusViewModels()
+    {
+        for(int i = 0; i<MaxPartyCount; i++)
+        {
+            CharacterStatusModel characterStatusModel = new CharacterStatusModel();
+            CharacterStatusViewModel characterStatusViewModel = new CharacterStatusViewModel(characterStatusModel);
+
+            characterStatusModel.SlotIndex = i;
+            _characterStatusModels[i] = characterStatusModel;
+            _characterStatusViewModels[i] = characterStatusViewModel;
+        }
+    }
+
+    public int GetMaxPartyCount()
+    {
+        return MaxPartyCount;
+    }
+
+    public CharacterStatusModel GetCharacterStatusModel(int slotIndex)
+    {
+        if(slotIndex < 0 || slotIndex >= MaxPartyCount)
+        {
+            Debug.LogError($"[CharacterStatusService] 파티 슬롯 범위를 벗어났습니다. SlotIndex: {slotIndex}");
+            return null;
+        }
+        return _characterStatusModels[slotIndex];
+    }
+}
