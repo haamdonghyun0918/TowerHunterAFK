2f711977da4921e09cc875a32f9acf985ad4a56b
지재욱 <145469443+wlwodnr@users.noreply.github.com>
2026-08-13 11:20:29 +0900

    [Feat] 스테이지 뷰 모델 생성

---
+++ Assets/02_Scripts/Ui/MVVM/StageViewModel.cs
@@ -0,0 +1,24 @@
+﻿public class StageViewModel : ViewModelBase
+{
+    private StageModel _stageModel;
+
+    public StageViewModel(StageModel StageModel)
+    {
+        _stageModel = StageModel;
+    }
+
+    public int CurrentStage
+    {
+        get => _stageModel.CurrentStage;
+
+        set
+        {
+            if(_stageModel.CurrentStage != value)
+            {
+                _stageModel.CurrentStage = value;
+
+                OnPropertyChanged(nameof(CurrentStage));
+            }
+        }
+    }
+}
