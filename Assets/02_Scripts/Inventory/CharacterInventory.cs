e68563406bbf25a73e5c388b69312d556164cc9b
함동현 <haamdonghyun0918@gmail.com>
2026-08-13 13:16:58 +0900

    [Feat] Character인벤토리(보관 및 저장), CharacterUtils(캐릭터에 대한 작업들) 구현 완료

---
+++ Assets/02_Scripts/Inventory/CharacterInventory.cs
@@ -0,0 +1,41 @@
+﻿using Cysharp.Threading.Tasks;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterInventory : MonoBehaviour
+{
+    public static CharacterInventory Instance { get; private set; }
+    private List<CharacterSaveData> _ownedCharacters = new List<CharacterSaveData>();
+    public event Action<List<CharacterSaveData>> OnCharacterChanged;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public UniTask Init()
+    {
+        if (SaveManager.Instance != null && SaveManager.Instance.CurrentSaveData != null)
+        {
+            _ownedCharacters = SaveManager.Instance.CurrentSaveData.OwnedCharacters;
+            OnCharacterChanged?.Invoke(_ownedCharacters);
+        }
+
+        Debug.Log("CharacterInventory 호출");
+        return UniTask.CompletedTask;
+    }
+
+    public List<CharacterSaveData> GetOwnedCharacters()
+    {
+        return _ownedCharacters;
+    }
+}
---
+++ Assets/02_Scripts/Utils/CharacterUtils.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+public class CharacterUtils : MonoBehaviour
+{
+    public static CharacterUtils Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void AddCharacters(string baseCharacterId)
+    {
+        CharacterData charData = GameDataManager.Instance.GetData<CharacterData>(baseCharacterId);
+
+        if (charData == null)
+        {
+            Debug.LogError($"[CharacterUtils] 존재하지 않는 캐릭터 ID입니다: {baseCharacterId}");
+            return;
+        }
+
+        SaveManager.Instance.CurrentSaveData.RecentCharacterUid += 1;
+        uint currentUid = SaveManager.Instance.CurrentSaveData.RecentCharacterUid;
+
+        CharacterSaveData newCharacter = new CharacterSaveData
+        {
+            UniqueId = "CH_" + currentUid.ToString(),
+            BaseId = baseCharacterId,
+            Level = 1,
+            Exp = 0,
+            EquippedWeaponUid = "",
+            EquippedArmorUid = "",
+            EquippedAccessoryUid = ""
+        };
+
+        SaveManager.Instance.CurrentSaveData.OwnedCharacters.Add(newCharacter);
+        SaveManager.Instance.SaveCurrentData();
+
+        Debug.Log($"[CharacterUtils] 헌터 획득! 헌터 이름: {charData.Name}], 게임 상 헌터 아이디: {newCharacter.UniqueId})");
+    }
+}
