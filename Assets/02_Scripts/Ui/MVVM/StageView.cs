387c9bce5014173c8d2cfd6b72f9cb2fd2d55e19
지재욱 <145469443+wlwodnr@users.noreply.github.com>
2026-08-13 22:43:55 +0900

    [Feat] 스테이지 뷰 생성 및 초기화

---
+++ Assets/02_Scripts/Ui/MVVM/StageView.cs
@@ -0,0 +1,32 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class StageView : MonoBehaviour
+{
+    [SerializeField] private TMP_Text Text_CurrentStage;
+
+    private StageViewModel _stageViewModel;
+
+    private void Start()
+    {
+        Init();
+    }
+
+    private void Init()
+    {
+        if(Text_CurrentStage == null)
+        {
+            Debug.LogError("[StageView]: Text_CurrentStage가 연결되지 않았습니다.");
+            return;
+        }
+
+        if(NetworkManager.Instance == null || NetworkManager.Instance.StageService == null)
+        {
+            Debug.LogError("[StageView]: StageService가 없습니다.");
+            return;
+        }
+
+        _stageViewModel = NetworkManager.Instance.StageService.GetStageViewModel();
+        
+    }
+}
