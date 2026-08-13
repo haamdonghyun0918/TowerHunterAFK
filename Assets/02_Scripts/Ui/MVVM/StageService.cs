36e61d9ff234191d9c5b70e1c21aed612d96804b
지재욱 <145469443+wlwodnr@users.noreply.github.com>
2026-08-13 11:21:10 +0900

    [Feat] StageService 뷰 모델 불러오기

---
+++ Assets/02_Scripts/Ui/MVVM/StageService.cs
@@ -0,0 +1,24 @@
+﻿
+public class StageService 
+{
+    private StageModel _stageModel;
+    private StageViewModel _stageViewModel;
+
+    public StageViewModel GetStageViewModel()
+    {
+        if(_stageViewModel == null )
+        {
+           CreateStageViewModel();
+        }
+        return _stageViewModel;
+    }
+
+    private void CreateStageViewModel()
+    {
+        var stageModel = new StageModel();
+        var stageViewModel = new StageViewModel(stageModel);
+
+        _stageViewModel = stageViewModel;
+        _stageModel = stageModel;
+    }
+}
