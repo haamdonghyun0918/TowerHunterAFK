72ea8f07a93c80bc5f905cd182291e54da5a271a
함동현 <haamdonghyun0918@gmail.com>
2026-08-17 13:06:00 +0900

    [Feat] UiManager, UiButton 스크립트 추가

---
+++ Assets/02_Scripts/Ui/UiButton.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UiButton : MonoBehaviour
+{
+    [SerializeField] private Button Button_Base;
+    [SerializeField] private Text Text_Base;
+    [SerializeField] private Image Image_Base;
+    [SerializeField] private Image Image_Select;
+
+    private void Awake()
+    {
+        InitUIButton();
+        SetDefaultUI();
+    }
+
+    private void OnEnable()
+    {
+        BindOnClickButtonEvent(OnClickSetSelectUI);
+    }
+
+    private void OnDisable()
+    {
+        Button_Base.onClick.RemoveAllListeners();
+    }
+
+    private void SetDefaultUI()
+    {
+        if (Image_Select != null)
+        {
+            Image_Select.gameObject.SetActive(false);
+        }
+    }
+
+    private void InitUIButton()
+    {
+        if (Button_Base != null)
+        {
+            return;
+        }
+        var button = this.gameObject.GetComponentInChildren<Button>();
+        if (button != null)
+        {
+            this.Button_Base = button;
+        }
+    }
+
+    public void BindOnClickButtonEvent(Action onClickCallback)
+    {
+        if (Button_Base == null) return;
+
+        Button_Base.onClick.AddListener(new UnityEngine.Events.UnityAction(onClickCallback));
+
+    }
+
+    public void UnBindOnClickButtonEvent(Action onClickCallback)
+    {
+        if (Button_Base == null) return;
+
+        Button_Base.onClick.RemoveListener(new UnityEngine.Events.UnityAction(onClickCallback));
+    }
+
+    public void ChangeButtonText(string buttonStr)
+    {
+        if (Text_Base == null) return;
+        Text_Base.text = buttonStr;
+    }
+
+    private void OnClickSetSelectUI()
+    {
+        if (Image_Select != null)
+        {
+            bool currentActive = Image_Select.gameObject.activeSelf;
+            Image_Select.gameObject.SetActive(!currentActive);
+        }
+    }
+}
