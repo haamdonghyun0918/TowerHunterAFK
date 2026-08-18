a44c18e531236430be0b59eaca4bf8c8f36da5aa
함동현 <haamdonghyun0918@gmail.com>
2026-08-18 21:35:44 +0900

    [Feat] SleepModeUi 프리팹화 및 스크립트 작성

---
+++ Assets/02_Scripts/Ui/SleepModeUi.cs
@@ -0,0 +1,115 @@
+﻿using UnityEngine;
+using TMPro;
+using System.ComponentModel;
+
+public class SleepModeUi : UiBase
+{
+    [Header("Ui Texts")]
+    [SerializeField] private TMP_Text Text_Floor;
+    [SerializeField] private TMP_Text Text_Gold;
+    [SerializeField] private TMP_Text Text_Diamond;
+    [SerializeField] private TMP_Text Text_Exp;
+    [SerializeField] private TMP_Text Text_MagicStone;
+
+    private StageViewModel _stageViewModel;
+    private PlayerResourceViewModel _playerResourceViewModel;
+
+    private void OnEnable()
+    {
+        Bind();
+        UpdateAllViews();
+    }
+
+    private void OnDisable()
+    {
+        UnBind();
+    }
+
+    private void Bind()
+    {
+        if (NetworkManager.Instance != null && NetworkManager.Instance.StageService != null)
+        {
+            _stageViewModel = NetworkManager.Instance.StageService.GetStageViewModel();
+
+            if (_stageViewModel != null)
+            {
+                _stageViewModel.PropertyChanged -= OnStagePropertyChanged;
+                _stageViewModel.PropertyChanged += OnStagePropertyChanged;
+            }
+        }
+
+        if (NetworkManager.Instance != null && NetworkManager.Instance.PlayerResourceService != null)
+        {
+            _playerResourceViewModel = NetworkManager.Instance.PlayerResourceService.GetPlayerResourceViewModel();
+
+            if (_playerResourceViewModel != null)
+            {
+                _playerResourceViewModel.PropertyChanged -= OnResourcePropertyChanged;
+                _playerResourceViewModel.PropertyChanged += OnResourcePropertyChanged;
+            }
+        }
+    }
+
+    private void UnBind()
+    {
+        if (_stageViewModel != null)
+        {
+            _stageViewModel.PropertyChanged -= OnStagePropertyChanged;
+        }
+
+        if (_playerResourceViewModel != null)
+        {
+            _playerResourceViewModel.PropertyChanged -= OnResourcePropertyChanged;
+        }
+    }
+
+    private void OnStagePropertyChanged(object sender, PropertyChangedEventArgs eventArgs)
+    {
+        if (eventArgs.PropertyName == nameof(StageViewModel.CurrentStage))
+        {
+            UpdateStageView();
+        }
+    }
+
+    private void OnResourcePropertyChanged(object sender, PropertyChangedEventArgs eventArgs)
+    {
+        if (eventArgs.PropertyName == nameof(PlayerResourceViewModel.Gold))
+        {
+            UpdateGoldView();
+        }
+        else if (eventArgs.PropertyName == nameof(PlayerResourceViewModel.Exp))
+        {
+            UpdateExpView();
+        }
+    }
+
+    private void UpdateAllViews()
+    {
+        UpdateStageView();
+        UpdateGoldView();
+        UpdateExpView();
+    }
+    private void UpdateStageView()
+    {
+        if (_stageViewModel != null && Text_Floor != null)
+        {
+            Text_Floor.text = $"{_stageViewModel.CurrentStage} Floor";
+        }
+    }
+
+    private void UpdateGoldView()
+    {
+        if (_playerResourceViewModel != null && Text_Gold != null)
+        {
+            Text_Gold.text = _playerResourceViewModel.Gold.ToString("N0");
+        }
+    }
+
+    private void UpdateExpView()
+    {
+        if (_playerResourceViewModel != null && Text_Exp != null)
+        {
+            Text_Exp.text = _playerResourceViewModel.Exp.ToString("N0");
+        }
+    }
+}
