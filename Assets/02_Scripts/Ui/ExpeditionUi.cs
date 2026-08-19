82f34ba9aaa438863fe45e81614680efb3020c33
함동현 <haamdonghyun0918@gmail.com>
2026-08-19 14:13:17 +0900

    [Feat] ExpeditionUi에서 닫힘 버튼 누르면 닫히도록 기능 추가 및, 원정대 단축키 Q로 설정

--- Assets/02_Scripts/Ui/ExpeditionUi.cs
+++ Assets/02_Scripts/Ui/ExpeditionUi.cs
@@ -2,5 +2,18 @@
 
 public class ExpeditionUi : UiBase
 {
+    [SerializeField] private UiButton _buttonClose;
 
+    private void OnEnable()
+    {
+        if (_buttonClose)
+        {
+            _buttonClose.BindOnClickButtonEvent(CloseExpedition);
+        }
+    }
+
+    private void CloseExpedition()
+    {
+        UiManager.Instance.CloseUi<ExpeditionUi>();
+    }
 }
