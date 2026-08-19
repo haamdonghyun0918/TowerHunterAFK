95d5f2959f4c68ad51a6807499d6fb7b838daec4
함동현 <haamdonghyun0918@gmail.com>
2026-08-19 19:20:36 +0900

    [Feat] ExpeditionList를 프리팹화하여 Ui에 동적생성하도록 함

---
+++ Assets/02_Scripts/Ui/ExpeditionList.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using TMPro;
+using System;
+
+public class ExpeditionList : MonoBehaviour
+{
+    [SerializeField] private TMP_Text _expeditionName;
+    [SerializeField] private UiButton _expeditionListButton;
+
+    private int _index;
+    private Action<int> _onClickList;
+
+    public void SetUp(ExpeditionData data, int index, Action<int> onClick)
+    {
+        _expeditionName.text = data.ExpeditionName;
+        _index = index;
+        _onClickList = onClick;
+
+        if (_expeditionListButton != null)
+        {
+            _expeditionListButton.BindOnClickButtonEvent(OnClickExpedition);
+        }
+
+    }
+
+    private void OnClickExpedition()
+    {
+        _onClickList?.Invoke(_index);
+    }
+
+}
