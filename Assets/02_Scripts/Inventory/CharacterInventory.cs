45cc96ba9f7fd26698998dd55677481ea7870999
함동현 <haamdonghyun0918@gmail.com>
2026-08-19 23:10:45 +0900

    [Fix] 헌터 인벤토리Ui 수정 수정 및 하드 코딩된 내용 삭제(가챠시스템과 연동하기 위함)

--- Assets/02_Scripts/Inventory/CharacterInventory.cs
+++ Assets/02_Scripts/Inventory/CharacterInventory.cs
@@ -12,36 +12,18 @@ public class CharacterInventory
         if (SaveManager.Instance != null && SaveManager.Instance.CurrentSaveData != null)
         {
             _ownedCharacters = SaveManager.Instance.CurrentSaveData.OwnedCharacters;
-            // 테스트용 하드코딩
-            if (_ownedCharacters.Count == 0)
-            {
-                GiveTestCharactersAndSetParty();
-            }
 
             if (OnCharacterChanged != null)
             {
                 OnCharacterChanged(_ownedCharacters);
             }
+
         }
-    }
-
-    private void GiveTestCharactersAndSetParty()
-    {
-        Debug.Log("[초기 설정] 헌터 3명 지급 및 기본 스쿼드 편성을 진행합니다.");
-
-        CharacterUtils charUtils = new CharacterUtils();
-
-        charUtils.AddCharacters("character_meteorshower_03");
-        charUtils.AddCharacters("character_frontspikesattack_06");
-        charUtils.AddCharacters("character_knives_02");
-
-        _ownedCharacters = SaveManager.Instance.CurrentSaveData.OwnedCharacters;
     }
 
     public List<CharacterSaveData> GetOwnedCharacters()
     {
         return _ownedCharacters;
     }
-
 
 }
