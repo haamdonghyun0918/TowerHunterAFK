9af3d6fa23cda8425d65caeab68bb45898b4abbd
함동현 <haamdonghyun0918@gmail.com>
2026-08-19 23:56:52 +0900

    [Feat] MainUi 버튼 사이즈 수정 및 장비 인벤토리 Ui 닫힘 버튼 기능 추가

--- Assets/02_Scripts/Ui/EquipmentInventoryUi.cs
+++ Assets/02_Scripts/Ui/EquipmentInventoryUi.cs
@@ -2,5 +2,18 @@
 
 public class EquipmentInventoryUi : UiBase
 {
+    [SerializeField] private UiButton _buttonClose;
 
+    private void OnEnable()
+    {
+        if (_buttonClose)
+        {
+            _buttonClose.BindOnClickButtonEvent(CloseEquipmentInventory);
+        }
+    }
+
+    private void CloseEquipmentInventory()
+    {
+        UiManager.Instance.CloseUi<EquipmentInventoryUi>();
+    }
 }
