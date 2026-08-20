613bf426c6d99c12cb426f0d94733d383a5b7cc8
지재욱 <145469443+wlwodnr@users.noreply.github.com>
2026-08-20 18:16:49 +0900

    [Feat] 캐릭터 코스트 보이도록 구현

--- Assets/02_Scripts/Ui/MVVM/CharacterStatusView.cs
+++ Assets/02_Scripts/Ui/MVVM/CharacterStatusView.cs
@@ -2,7 +2,7 @@
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
-
+using System.Collections.Generic;
 public class CharacterStatusView : MonoBehaviour
 {
     [SerializeField] private int _slotIndex;
@@ -12,14 +12,16 @@ public class CharacterStatusView : MonoBehaviour
 
     private CharacterStatusViewModel _characterStatusViewModel;
 
+    [SerializeField] private List<GameObject> SkillCostList = new List<GameObject>();
+
     private void Start()
     {
-        Init();   
+        Init();
     }
 
     private void OnEnable()
     {
-        if(_characterStatusViewModel != null)
+        if (_characterStatusViewModel != null)
         {
             Bind();
             UpdateView();
@@ -52,7 +54,7 @@ public class CharacterStatusView : MonoBehaviour
 
         _characterStatusViewModel = NetworkManager.Instance.CharacterStatusService.GetCharacterStatusViewModel(_slotIndex);
 
-        if(_characterStatusViewModel == null)
+        if (_characterStatusViewModel == null)
         {
             Debug.LogError($"[CharacterStatusView] CharacterStatusViewModel을 가져오지 못했습니다. SlotIndex: {_slotIndex}");
             return;
@@ -66,7 +68,7 @@ public class CharacterStatusView : MonoBehaviour
 
     private void Bind()
     {
-        if(_characterStatusViewModel == null)
+        if (_characterStatusViewModel == null)
         {
             return;
         }
@@ -77,7 +79,7 @@ public class CharacterStatusView : MonoBehaviour
 
     private void UnBind()
     {
-        if(_characterStatusViewModel == null)
+        if (_characterStatusViewModel == null)
         {
             return;
         }
@@ -86,10 +88,12 @@ public class CharacterStatusView : MonoBehaviour
 
     private void OnPropertyChanged(object sender, PropertyChangedEventArgs eventArgs)
     {
-        if(eventArgs.PropertyName == nameof(CharacterStatusViewModel.CurrentHp) 
-            || eventArgs.PropertyName == nameof(CharacterStatusViewModel.MaxHp) 
-            || eventArgs.PropertyName == nameof(CharacterStatusViewModel.HpRatio) 
-            || eventArgs.PropertyName == nameof(CharacterStatusViewModel.IsActive))
+        if (eventArgs.PropertyName == nameof(CharacterStatusViewModel.CurrentHp)
+            || eventArgs.PropertyName == nameof(CharacterStatusViewModel.MaxHp)
+            || eventArgs.PropertyName == nameof(CharacterStatusViewModel.HpRatio)
+            || eventArgs.PropertyName == nameof(CharacterStatusViewModel.IsActive)
+            || eventArgs.PropertyName == nameof(CharacterStatusViewModel.CurrentSkillCost)
+            || eventArgs.PropertyName == nameof(CharacterStatusViewModel.MaxSkillCost))
         {
             UpdateView();
         }
@@ -97,17 +101,36 @@ public class CharacterStatusView : MonoBehaviour
 
     private void UpdateView()
     {
-        if(_characterStatusViewModel == null || Root_CharacterStatus == null || Slider_Hp == null)
+        if (_characterStatusViewModel == null || Root_CharacterStatus == null || Slider_Hp == null)
         {
             return;
         }
 
         Root_CharacterStatus.SetActive(_characterStatusViewModel.IsActive);
 
-        if(_characterStatusViewModel.IsActive == false)
+        if (_characterStatusViewModel.IsActive == false)
         {
             return;
         }
         Slider_Hp.normalizedValue = Mathf.Clamp01(_characterStatusViewModel.HpRatio);
+
+        UpdateSkillCostView();
+    }
+
+    private void UpdateSkillCostView()
+    {
+        int activeCostCount = Mathf.Clamp(_characterStatusViewModel.CurrentSkillCost, 0, Mathf.Min(_characterStatusViewModel.MaxSkillCost, SkillCostList.Count));
+
+        for(int i = 0; i< SkillCostList.Count; i++)
+        {
+            if(SkillCostList[i] == null)
+            {
+                continue;
+            }
+
+            bool shouldBeActive = i < activeCostCount;
+
+            SkillCostList[i].SetActive(shouldBeActive);
+        }
     }
 }
