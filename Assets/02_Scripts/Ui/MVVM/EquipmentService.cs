dea069ccbdc50a6ba719eb4f82677152117044f3
지재욱 <145469443+wlwodnr@users.noreply.github.com>
2026-08-23 01:28:47 +0900

    [Fix] Equipment MVVM 구조 개편 중 - EquipmentService 수정 - 초기화

--- Assets/02_Scripts/Ui/MVVM/EquipmentService.cs
+++ Assets/02_Scripts/Ui/MVVM/EquipmentService.cs
@@ -1,210 +1,63 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
 
 public class EquipmentService
 {
-    private EquipmentDetailViewModel _equipmentDetailViewModel;
+    private readonly Dictionary<string, EquipmentModel> _equipmentModelDict = new Dictionary<string, EquipmentModel>();
 
-    public EquipmentDetailViewModel GetEquipmentDetailViewModel()
+    public event Action<string> EquipmentChanged;
+    public event Action EquipmentInventoryChanged;
+
+    public string DismantleRewardCurrencyName => "Gold";
+
+    public bool TryGetEuipmentModel(string uniqueId, out EquipmentModel equipmentModel)
     {
-        if (_equipmentDetailViewModel == null)
+        equipmentModel = null;
+
+        if(string.IsNullOrEmpty(uniqueId))
         {
-            _equipmentDetailViewModel = new EquipmentDetailViewModel();
-        }
-        return _equipmentDetailViewModel;
-    }
-
-    private EquipmentEnhanceViewModel _equipmentEnhanceViewModel;
-
-    public EquipmentEnhanceViewModel GetEquipmentEnhanceViewModel()
-    {
-        if (_equipmentEnhanceViewModel == null)
-        {
-            _equipmentEnhanceViewModel = new EquipmentEnhanceViewModel();
-        }
-        return _equipmentEnhanceViewModel;
-    }
-
-    private EquipmentDisassembleViewModel _equipmentDisassembleViewModel;
-
-    public EquipmentDisassembleViewModel GetEquipmentDisassembleViewModel()
-    {
-        if (_equipmentDisassembleViewModel == null)
-        {
-            _equipmentDisassembleViewModel = new EquipmentDisassembleViewModel();
-        }
-        return _equipmentDisassembleViewModel;
-    }
-
-    public void SetDetailTarget(string uniqueId)
-    {
-        if (SaveManager.Instance.EquipmentDict.TryGetValue(uniqueId, out EquipmentSaveData saveData) == false)
-        {
-            Debug.LogError($"[EquipmentService] 해당 UID의 장비를 찾을 수 없습니다: {uniqueId}");
-            return;
+            Debug.LogError($"[EquipmentService]: 장비 uniqueId가 비어있습니다.");
+            return false;
         }
 
-        EquipMentData baseData = GameDataManager.Instance.GetData<EquipMentData>(saveData.BaseId);
-        if (baseData == null) return;
-
-        var vm = GetEquipmentDetailViewModel();
-
-        vm.TargetEquipmentUniqueId = uniqueId;
-        vm.ItemIconAddress = baseData.IconAddress;
-        vm.ItemName = baseData.Name;
-
-        int totalAtk = baseData.BuffAtk + (saveData.EnhanceLevel * 5);
-        int totalDef = baseData.BuffDef + (saveData.EnhanceLevel * 3);
-        int totalSpd = baseData.BuffAtkSpeed;
-
-        vm.TotalStatText = $"공격력 : {totalAtk}\n방어력 : {totalDef}\n속도 : {totalSpd}";
-    }
-
-    public void SetEnhanceTarget(string uniqueId)
-    {
-        if (SaveManager.Instance.EquipmentDict.TryGetValue(uniqueId, out EquipmentSaveData saveData) == false)
+        if(_equipmentModelDict.TryGetValue(uniqueId, out equipmentModel))
         {
-            Debug.LogError($"[EquipmentService] 해당 UID의 장비를 찾을 수 없습니다: {uniqueId}");
-            return;
+            return true;
         }
 
-        EquipMentData baseData = GameDataManager.Instance.GetData<EquipMentData>(saveData.BaseId);
-
-        if (baseData == null)
+        if(SaveManager.Instance == null || SaveManager.Instance.CurrentSaveData == null)
         {
-            Debug.LogError($"[EquipmentService] 해당 baseData의 장비를 찾을 수 없습니다: {baseData}");
-            return;
+            Debug.LogError("[EquipmentService] SaveManager가 초기화되지 않았습니다.");
+            return false;
         }
 
-        var vm = GetEquipmentEnhanceViewModel();
-
-        vm.TargetEquipmentUniqueId = uniqueId;
-        vm.ItemIconAddress = baseData.IconAddress;
-        vm.ItemName = baseData.Name;
-        vm.EnhanceLevel = saveData.EnhanceLevel;
-
-        int totalAtk = baseData.BuffAtk + (saveData.EnhanceLevel * 5);
-        vm.TotalAtkText = $"공격력: {totalAtk}";
-
-        int totalDef = baseData.BuffDef + (saveData.EnhanceLevel * 3);
-        vm.TotalDefText = $"방어력: {totalDef}";
-
-        //테스트를 위해 일단은 골드만 요구하도록 로직 구현
-        long cost = (saveData.EnhanceLevel + 1) * 10;
-        vm.CostText = $"{cost} Gold";
-
-
-        //==================테스트용 하드코딩 데이터.
-        //사용시 본 메서드의 위쪽 코드 전부 주석처리 하고 사용할 것.
-        //아이콘 주소가 없어서 에러가 날 수도 있음.
-        //var vm = GetEquipmentEnhanceViewModel();
-
-        //vm.TargetEquipmentUniqueId = uniqueId;
-        //vm.ItemName = "테스트 장비";
-        //vm.EnhanceLevel = 5;
-        //vm.TotalAtkText = "공격력: 100";
-        //vm.CostText = "100 Gold";
-    }
-
-    public bool RequestEnhance(string uniqueId)
-    {
-        if (SaveManager.Instance.EquipmentDict.TryGetValue(uniqueId, out EquipmentSaveData saveData) == false)
+        if(SaveManager.Instance.EquipmentDict.TryGetValue(uniqueId, out EquipmentSaveData saveData) == false )
         {
             Debug.LogError($"[EquipmentService] 해당 UID의 장비를 찾을 수 없습니다: {uniqueId}");
             return false;
         }
 
-        EquipMentData baseData = GameDataManager.Instance.GetData<EquipMentData>(saveData.BaseId);
-        if (baseData == null)
+        if(GameDataManager.Instance == null)
         {
-            Debug.LogError($"[EquipmentService] 해당 장비의 BaseData를 찾을 수 없습니다: {saveData.BaseId}");
+            Debug.LogError("[EquipmentService]: GameDataManager가 없습니다.");
+
             return false;
         }
 
-        long cost = (saveData.EnhanceLevel + 1) * 10;
-        bool isGoldUsed = NetworkManager.Instance.PlayerResourceService.RequestUseGold(cost);
+        EquipmentData baseData = GameDataManager.Instance.GetData<EquipmentData>(saveData.BaseId);
 
-        if (isGoldUsed == false)
+        if(baseData == null )
         {
-            Debug.LogWarning("골드가 부족합니다.");
+            Debug.LogError($"[EquipmentService] 장비 원본 데이터를 찾을 수 없습니다. BaseId: {saveData.BaseId}");
+
             return false;
         }
 
-        saveData.EnhanceLevel += 1;
+        equipmentModel = new EquipmentModel(saveData, baseData);
 
-        SetEnhanceTarget(uniqueId);
-
-        SaveManager.Instance.SaveCurrentData();
-
-        Debug.Log("강화 성공");
+        _equipmentModelDict.Add(uniqueId, equipmentModel);
 
         return true;
     }
-
-    public void SetDisassembleTarget(string uniqueId)
-    {
-        if (SaveManager.Instance.EquipmentDict.TryGetValue(uniqueId, out EquipmentSaveData saveData) == false)
-        {
-            Debug.LogError($"[EquipmentService] 해당 UID의 장비를 찾을 수 없습니다: {uniqueId}");
-            return;
-        }
-
-        EquipMentData baseData = GameDataManager.Instance.GetData<EquipMentData>(saveData.BaseId);
-
-        if (baseData == null)
-        {
-            Debug.LogError($"[EquipmentService] 해당 baseData의 장비를 찾을 수 없습니다: {baseData}");
-            return;
-        }
-
-        var vm = GetEquipmentDisassembleViewModel();
-
-        vm.TargetEquipmentUniqueId = uniqueId;
-        vm.ItemIconAddress = baseData.IconAddress;
-        vm.ItemName = $"{baseData.Name}+{saveData.EnhanceLevel}";
-
-        //테스트용 임시 획득 재화(골드) 나중에 마석 관련 데이터, 로직 나오면 바꿀 것.
-        long reward = (baseData.Price) / 10;
-        vm.RewardText = $"장비 분해 시 획득 재화\n{reward} Gold\n(테스트용 골드 지급/추후 마석으로 바꿀 것)";
-        
-    }
-
-    public bool RequestDisassemble(string uniqueId)
-    {
-        if (SaveManager.Instance.EquipmentDict.TryGetValue(uniqueId, out EquipmentSaveData saveData) == false)
-        {
-            Debug.LogError($"[EquipmentService] 해당 UID의 장비를 찾을 수 없습니다: {uniqueId}");
-            return false;
-        }
-
-        EquipMentData baseData = GameDataManager.Instance.GetData<EquipMentData>(saveData.BaseId);
-        if (baseData == null)
-        {
-            Debug.LogError($"[EquipmentService] 해당 장비의 BaseData를 찾을 수 없습니다: {saveData.BaseId}");
-            return false;
-        }
-
-        long reward = (baseData.Price) / 10;
-
-        //이게 장비 아이템 제거하는 함수인지 불명확함. 물어볼것.
-        NetworkManager.Instance.PlayerResourceService.RequestUseEquipments(saveData.UniqueId);
-
-        //이건 직접 세이브 매니저를 통해서 제거하는 코드인데
-        //인벤토리 통해서 제거하는게 나을 것 같으니 테스트 용에서만 사용할것.
-        //SaveManager.Instance.EquipmentDict.Remove(uniqueId);
-        //var equipList = SaveManager.Instance.CurrentSaveData.OwnedEquipments;
-        //var targetEquip = equipList.Find(e => e.UniqueId == uniqueId);
-        //if (targetEquip != null)
-        //{
-        //    equipList.Remove(targetEquip);
-        //}
-
-        NetworkManager.Instance.PlayerResourceService.RequestAddGold(reward);
-
-        SaveManager.Instance.SaveCurrentData();
-
-        Debug.Log("분해 성공");
-
-        return true;
-    }
-
 }
