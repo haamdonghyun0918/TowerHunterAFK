26219462f1b9ef95eeb910993c6a2b1ec25b36e2
권성혁 <gkrrpdl@gmail.com>
2026-08-24 15:57:59 +0900

    [Add] 캐릭터 레벨용 순수C# 클래스 작성

---
+++ Assets/02_Scripts/GameObject/Character/CharacterLevelManager.cs
@@ -0,0 +1,38 @@
+﻿public class CharacterLevelManager
+{
+    private int _maxExp = 100000;
+    private int _currentExp;
+    private int _needExpForLevelUp = 2000;
+
+    public int GetCurrentExp()
+    {
+        return _currentExp;
+    }
+
+    public int GetMaxExp()
+    {
+        return _maxExp;
+    }
+
+    public void AddExp(int amount)
+    {
+        _currentExp += amount;
+
+        if (_currentExp >= _maxExp)
+        {
+            _currentExp = _maxExp;
+        }
+    }
+
+    public int UseExpForLevelUp()
+    {
+        if (_currentExp < _needExpForLevelUp)
+        {
+            return 0;
+        }
+
+        _currentExp -= _needExpForLevelUp;
+
+        return _needExpForLevelUp;
+    }
+}
