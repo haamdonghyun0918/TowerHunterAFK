5af4803c0196db05f1e132b744d7f2ce27715c16
이경인 <031317ki@gmail.com>
2026-08-24 17:53:06 +0900

    [Fix] PlayerPartyControllerBase에 중복 로직 이동

--- Assets/02_Scripts/Battle/PlayerPartyControllerForBoss.cs
+++ Assets/02_Scripts/Battle/PlayerPartyControllerForBoss.cs
@@ -4,97 +4,14 @@ using UnityEngine;
 public class PlayerPartyControllerForBoss : PlayerPartyControllerBase
 {
     private float moveSpeed = 10.0f;
-
     public bool _isBattling = false;
     public bool _isMovable = false;
 
-    public Transform[] _playerSlots = new Transform[5];
-    private Character[] _hunters = new Character[5];
-
-    
     private void Update()
     {
         if ((_isBattling == false) && (_isMovable == true))
         {
             transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
-        }
-    }
-
-    public Character GetHunter(int index)
-    {
-        if ((index >= 0) && (index < _hunters.Length))
-        {
-            return _hunters[index];
-        }
-        else
-        {
-            Debug.LogError($"[PlayerPartyController] GetHunter: index {index}는 범위를 초과함!");
-            return null;
-        }
-    }
-
-    public int GetCurrentHunterCount()
-    {
-        int count = 0;
-
-        foreach (var hunter in _hunters)
-        {
-            if ((hunter != null) && (hunter._isDead == false))
-            {
-                count++;
-            }
-        }
-
-        return count;
-    }
-
-    public bool AddHunter(Character newHunter)
-    {
-        if (newHunter == null)
-        {
-            Debug.LogError($"[PlayerPartyController] AddHunter: newHunter가 NULL입니다.");
-            return false;
-        }
-
-        for (int i = 0; i < _hunters.Length; i++)
-        {
-            if (_hunters[i] == null)
-            {
-                _hunters[i] = newHunter;
-                newHunter.transform.SetParent(_playerSlots[i]);
-                newHunter.transform.localPosition = Vector3.zero;
-                newHunter.transform.localRotation = Quaternion.identity;
-                //newHunter.OnHunterDeath += HandleHunterDeath;
-
-                return true;
-            }
-        }
-
-        return false;
-    }
-
-    public void MakeFullHPAllHunters()
-    {
-        foreach (var hunter in _hunters)
-        {
-            if (hunter != null)
-            {
-                Debug.Log($"[PlayerPartyController] MakeFullHPAllHunters: {hunter.name}의 HP를 회복합니다.");
-                hunter.MakeFullHp();
-            }
-        }
-    }
-
-    public void HandleHunterDeath(Character deadHunter)
-    {
-        for (int i = 0; i < _hunters.Length; i++)
-        {
-            if (_hunters[i] == deadHunter)
-            {
-                _hunters[i] = null;
-                //newHunter.OnHunterDeath -= HandleHunterDeath;
-                break;
-            }
         }
     }
 
@@ -107,11 +24,6 @@ public class PlayerPartyControllerForBoss : PlayerPartyControllerBase
 
             GameObject encounteredMonsterParty = other.gameObject;
             BattleManager.Instance.StartBattle(this, encounteredMonsterParty);
-        }
-        else if (other.CompareTag("ClearSpot"))
-        {
-            _isMovable = false;
-            MapManager.Instance.ClearedCurrentStage();
         }
     }
 }
