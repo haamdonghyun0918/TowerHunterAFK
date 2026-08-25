f52b55f1faf4da34f45288f3ac862968c6412ed8
함동현 <haamdonghyun0918@gmail.com>
2026-08-25 11:40:43 +0900

    [Feat] EnhanceWarningUi 스크립트 추가및 연결

---
+++ Assets/02_Scripts/Ui/EnhanceWarningUi.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System;
+
+public class EnhanceWarningUi : UiBase
+{
+    [SerializeField] private UiButton _buttonConfirm;
+    [SerializeField] private UiButton _buttonCancel;
+
+    public event Action _onConfirmAction;
+
+    private void OnEnable()
+    {
+        if (_buttonConfirm != null)
+        {
+            _buttonConfirm.UnBindOnClickButtonEvent(OnClickConfirm);
+            _buttonConfirm.BindOnClickButtonEvent(OnClickConfirm);
+        }
+
+        if (_buttonCancel != null)
+        {
+            _buttonCancel.UnBindOnClickButtonEvent(OnClickCancel);
+            _buttonCancel.BindOnClickButtonEvent(OnClickCancel);
+        }
+    }
+
+    private void OnDisable()
+    {
+        if (_buttonConfirm != null)
+        {
+            _buttonConfirm.UnBindOnClickButtonEvent(OnClickConfirm);
+        }
+
+        if (_buttonCancel != null)
+        {
+            _buttonCancel.UnBindOnClickButtonEvent(OnClickCancel);
+        }
+
+        _onConfirmAction = null;
+    }
+
+    public void SetUp(Action onConfirm)
+    {
+        _onConfirmAction = onConfirm;
+    }
+
+    private void OnClickConfirm()
+    {
+        _onConfirmAction?.Invoke();
+        UiManager.Instance.CloseUi<EnhanceWarningUi>();
+    }
+
+    private void OnClickCancel()
+    {
+        UiManager.Instance.CloseUi<EnhanceWarningUi>();
+        UiManager.Instance.CloseUi<HunterInfoUi>();
+    }
+}
