60eea38619d6681796dc6655a9ebe835934a3bfe
이경인 <031317ki@gmail.com>
2026-08-26 16:29:08 +0900

    [Feat] BossRaidBattleUI 추가

---
+++ Assets/02_Scripts/Ui/BossRaidBattleUI.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class BossRaidBattleUI : UiBase
+{
+    [SerializeField] private Button Button_ExitBossRaid;
+
+    private void OnEnable()
+    {
+        BindButton();
+    }
+
+    private void BindButton()
+    {
+        Button_ExitBossRaid.onClick.AddListener(OnClick_RxitBossRaidButton);
+    }
+
+
+    public void OnClick_RxitBossRaidButton()
+    {
+        UiManager.Instance.CloseUi<BossRaidBattleUI>();
+        UiManager.Instance.OpenUi<MainUi>();
+    }
+
+
+}
