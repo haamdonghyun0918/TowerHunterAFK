255cc07d858ff6514183f5ad82504f10f20803bb
이경인 <031317ki@gmail.com>
2026-08-26 18:06:55 +0900

    [Feat] 보스 레이트 전투 UI 생성

--- Assets/02_Scripts/Ui/BossRaidBattleUI.cs
+++ Assets/02_Scripts/Ui/BossRaidBattleUI.cs
@@ -5,6 +5,11 @@ public class BossRaidBattleUI : UiBase
 {
     [SerializeField] private Button Button_ExitBossRaid;
 
+    private void Awake()
+    {
+        
+    }
+
     private void OnEnable()
     {
         BindButton();
@@ -18,8 +23,12 @@ public class BossRaidBattleUI : UiBase
 
     public void OnClick_RxitBossRaidButton()
     {
+        //BattleManager.Instance.EndBossBattle();
         UiManager.Instance.CloseUi<BossRaidBattleUI>();
         UiManager.Instance.OpenUi<MainUi>();
+        //보스 배틀 끝내는 로직 실행시킬것.
+
+
     }
 
 
