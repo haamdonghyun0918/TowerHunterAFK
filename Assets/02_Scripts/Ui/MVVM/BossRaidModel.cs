b2e63973f7b55205a34688292401ec76330cf2cb
지재욱 <145469443+wlwodnr@users.noreply.github.com>
2026-08-27 15:43:58 +0900

    [Feat] 보스 레이드 모델 추가

---
+++ Assets/02_Scripts/Ui/MVVM/BossRaidModel.cs
@@ -0,0 +1,7 @@
+﻿public class BossRaidModel
+{
+    public const int MaxPartySize = 5;
+    public BossData SelectedBoss { get; set; }
+    public bool IsRaidInProgress { get; set; }
+    public string[] PartyUids { get; } = new string[MaxPartySize] { "", "", "", "", "" };
+}
