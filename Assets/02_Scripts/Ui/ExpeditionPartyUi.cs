6cf5a1b561deefdf5afc17578799ed91505885b1
함동현 <haamdonghyun0918@gmail.com>
2026-08-28 10:42:11 +0900

    [Feat] 원정 헌터 인벤토리에서도 탑 인벤토리와 똑같이 정렬 및 실시간 변동되도록 수정

--- Assets/02_Scripts/Ui/ExpeditionPartyUi.cs
+++ Assets/02_Scripts/Ui/ExpeditionPartyUi.cs
@@ -32,6 +32,19 @@ public class ExpeditionPartyUi : UiBase
             _buttonCheck.UnBindOnClickButtonEvent(ConfirmPartySetting);
             _buttonCheck.BindOnClickButtonEvent(ConfirmPartySetting);
         }
+
+        HunterInfoUi.OnHunterStateChanged -= OnHunterStateChanged;
+        HunterInfoUi.OnHunterStateChanged += OnHunterStateChanged;
+    }
+
+    private void OnDisable()
+    {
+        HunterInfoUi.OnHunterStateChanged -= OnHunterStateChanged;
+    }
+
+    private void OnHunterStateChanged()
+    {
+        ReLoadExpeditionPartyUi().Forget();
     }
 
     public async UniTaskVoid ReLoadExpeditionPartyUi()
@@ -93,6 +106,8 @@ public class ExpeditionPartyUi : UiBase
             }
         }
 
+        waitChars.Sort(CompareHunterSpec);
+
         int targetSlotCount = Mathf.Max(_firstSlotCount, waitChars.Count);
         int currentCount = _createdSlots.Count;
 
@@ -114,6 +129,41 @@ public class ExpeditionPartyUi : UiBase
             {
                 _createdSlots[i].gameObject.SetActive(false);
             }
+        }
+    }
+
+    private int CompareHunterSpec(CharacterSaveData huntData, CharacterSaveData realHuntData)
+    {
+        int rankComparison = realHuntData.Rank.CompareTo(huntData.Rank);
+        if (rankComparison != 0)
+        {
+            return rankComparison;
+        }
+
+        int expComparison = realHuntData.Exp.CompareTo(huntData.Exp);
+        if (expComparison != 0)
+        {
+            return expComparison;
+        }
+
+        var dataA = GameDataManager.Instance.GetData<CharacterData>(huntData.BaseId);
+        var dataB = GameDataManager.Instance.GetData<CharacterData>(realHuntData.BaseId);
+
+        int rarityValueA = dataA != null ? GetRarityValue(dataA.Rarity) : 0;
+        int rarityValueB = dataB != null ? GetRarityValue(dataB.Rarity) : 0;
+
+        return rarityValueB.CompareTo(rarityValueA);
+    }
+
+    private int GetRarityValue(string rarity)
+    {
+        switch (rarity)
+        {
+            case "S": return 4;
+            case "A": return 3;
+            case "B": return 2;
+            case "C": return 1;
+            default: return 0;
         }
     }
 
