cfb0c9dc9f5246e6d4301defc003a5274c95f6ed
권성혁 <gkrrpdl@gmail.com>
2026-08-28 15:52:51 +0900

    [Fix] 스킬, 게임데이터 클래스 사용하지않는 using 삭제

--- Assets/02_Scripts/Data/GameData.cs
+++ Assets/02_Scripts/Data/GameData.cs
@@ -1,8 +1,4 @@
-﻿using JetBrains.Annotations;
-using System;
-using System.Collections.Generic;
-
-[System.Serializable]
+﻿[System.Serializable]
 public class GameDataBase
 {
     public string Id;
--- Assets/02_Scripts/Skill/Skill.cs
+++ Assets/02_Scripts/Skill/Skill.cs
@@ -1,5 +1,4 @@
 ﻿using Cysharp.Threading.Tasks;
-using Cysharp.Threading.Tasks.Triggers;
 using System.Collections.Generic;
 using System.Threading;
 using UnityEngine;
